3e62ed2eb2584b3357fa150fb004f0452b37140b
Stepan <git@programydovoleb.cz>
2024-12-30 17:31:46 +0100

    Aktualizace

--- src/components/footer/src/script.js
+++ src/components/footer/src/script.js
@@ -7,15 +7,21 @@ export default {
 	data: function () {
 		return {
 			list: {
-				tw: "https://www.twitter.com/programydovoleb",
-				ig: "https://www.instagram.com/programydovoleb",
+				// email: "mailto:info@programydovoleb.cz",
 				threads: "https://www.threads.net/@programydovoleb",
+				ig: "https://www.instagram.com/programydovoleb",
 				tiktok: "https://www.tiktok.com/@programydovoleb",
-				fb: "https://www.facebook.com/programydovoleb",
-				reddit: "https://www.reddit.com/r/volby/",
+				// fb: "https://www.facebook.com/programydovoleb",
+				// reddit: "https://www.reddit.com/r/volby/",
 				bsky: "https://bsky.app/profile/programydovoleb.cz",
-				mastodon: "https://cztwitter.cz/@programydovoleb",
-				// linkedin: "https://www.linkedin.com/company/programydovoleb"
+				tw: "https://www.twitter.com/programydovoleb",
+				// mastodon: "https://cztwitter.cz/@programydovoleb",
+				// linkedin: "https://www.linkedin.com/company/programydovoleb",
+				// yt: "https://youtube.com/@programydovoleb",
+				// discord: "https://discord.gg/fVUmJ7wXzc",
+				// rss: "https://programydovoleb.cz/rss",
+				// whatsapp: "https://chat.whatsapp.com/HsQIv3iE6Up3Lqnj52ZAlX",
+				// signal: "https://signal.me/#eu/xlLftS8PndqPElvvhbXV6qGoXhyUw4DfPTg7A0U62_yrnWDQpJ5K0pzz6gAw-ue7"
 			}
 		}
 	}
--- src/views/about/kontakty/src/script.js
+++ src/views/about/kontakty/src/script.js
@@ -14,12 +14,12 @@ export default {
 				{link: "https://www.threads.net/@programydovoleb", icon: "threads", label: "Threads", sub: "", count: 1100},
 				{link: "https://tiktok.com/@programydovoleb", icon: "tiktok", label: "TikTok", sub: "", count: 660},
 				{link: "https://www.facebook.com/programydovoleb", icon: "fb", label: "Facebook", sub: "", count: 640},
-				{link: "https://bsky.app/profile/programydovoleb.cz", icon: "bsky", label: "BlueSky", sub: "", count: 500},
+				{link: "https://bsky.app/profile/programydovoleb.cz", icon: "bsky", label: "Bluesky", sub: "", count: 500},
 				{link: "https://www.reddit.com/u/programydovoleb/", icon: "reddit", label: "Reddit", sub: "", count: 410},
 				{link: "https://cztwitter.cz/@programydovoleb", icon: "mastodon", label: "Mastodon", sub: "", count: 220},
 				{link: "https://www.linkedin.com/company/programydovoleb", icon: "linkedin", label: "LinkedIn", sub: "", count: 20},
 				{link: "https://youtube.com/@programydovoleb", icon: "yt", label: "Youtube", sub: "nové", count: 0},
-				{link: "https://discord.gg/fVUmJ7wXzc", icon: "discord", label: "Discord", sub: "nové", count: 0}
+				// {link: "https://discord.gg/fVUmJ7wXzc", icon: "discord", label: "Discord", sub: "nové", count: 0}
 			]
 		}
 	},
