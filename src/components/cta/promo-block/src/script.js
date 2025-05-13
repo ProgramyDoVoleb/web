c64bd5c375df7922d7eef14baca26640d63f51e6
Stepan <git@programydovoleb.cz>
2025-05-13 10:42:52 +0200

    Rychlé akce na homepage

--- src/components/cta/promo-block/src/script.js
+++ src/components/cta/promo-block/src/script.js
@@ -1,3 +1,25 @@
+import EditableBasic from '@/components/editable/basic/do.vue';
+import EditableSuggest from '@/components/editable/suggest/do.vue';
+import EditableImage from '@/components/editable/image/do.vue';
+import PopUp from '@/components/pop-up/do.vue';
+
 export default {
-	name: 'current-election-highlight-block'
+	name: 'current-election-highlight-block',
+	components: {
+		EditableBasic, EditableSuggest, EditableImage, PopUp
+	},
+	data: function () {
+		return {
+			mobile: false
+		}
+	},
+	mounted: function () {
+		if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+			// true for mobile device
+			this.mobile = true;
+		  }else{
+			// false for not mobile device
+			this.mobile = false;
+		  }
+	}
 };
