72e1ebf2213d5de0bbcdf5f14586bc352ad872c5
Stepan <git@programydovoleb.cz>
2026-01-06 16:26:30 +0100

    Drobné opravy

--- src/views/about/kontakty/src/script.js
+++ src/views/about/kontakty/src/script.js
@@ -9,18 +9,18 @@ export default {
 		return {
 			cdn,
 			social: [
-				// {link: "https://www.twitter.com/programydovoleb", icon: "tw", label: "Twitter / X", sub: "", count: 4640},
-				{link: "https://www.instagram.com/programydovoleb", icon: "ig", label: "Instagram", sub: "", count: 1710},
-				{link: "https://www.threads.net/@programydovoleb", icon: "threads", label: "Threads", sub: "", count: 1100},
-				{link: "https://tiktok.com/@programydovoleb", icon: "tiktok", label: "TikTok", sub: "", count: 660},
-				{link: "https://www.facebook.com/programydovoleb", icon: "fb", label: "Facebook", sub: "", count: 640},
-				{link: "https://bsky.app/profile/programydovoleb.bsky.social", icon: "bsky", label: "Bluesky", sub: "", count: 500},
-				{link: "https://www.reddit.com/u/programydovoleb/", icon: "reddit", label: "Reddit", sub: "", count: 410},
-				{link: "https://cztwitter.cz/@programydovoleb", icon: "mastodon", label: "Mastodon", sub: "", count: 220},
-				{link: "https://www.linkedin.com/company/programydovoleb", icon: "linkedin", label: "LinkedIn", sub: "", count: 20},
-				{link: "https://open.spotify.com/show/0t3PQ53sL3JkydArnDCTZZ", icon: "spotify", label: "Spotify", sub: "od července", count: 0},
-				{link: "https://youtube.com/@programydovoleb", icon: "yt", label: "Youtube", sub: "od července", count: 0},
-				// {link: "https://discord.gg/fVUmJ7wXzc", icon: "discord", label: "Discord", sub: "nové", count: 0}
+				// {link: "https://www.twitter.com/programydovoleb", icon: "tw", label: "Twitter / X", sub: "", count: [0,4640]},
+				{link: "https://www.instagram.com/programydovoleb", icon: "ig", label: "Instagram", sub: "", count: [5450,1710]},
+				{link: "https://www.threads.net/@programydovoleb", icon: "threads", label: "Threads", sub: "", count: [2060,1100]},
+				{link: "https://tiktok.com/@programydovoleb", icon: "tiktok", label: "TikTok", sub: "", count: [1430,660]},
+				{link: "https://www.facebook.com/programydovoleb", icon: "fb", label: "Facebook", sub: "", count: [850,640]},
+				{link: "https://bsky.app/profile/programydovoleb.bsky.social", icon: "bsky", label: "Bluesky", sub: "", count: [1100,500]},
+				{link: "https://www.reddit.com/u/programydovoleb/", icon: "reddit", label: "Reddit", sub: "", count: [1630,410]},
+				{link: "https://cztwitter.cz/@programydovoleb", icon: "mastodon", label: "Mastodon", sub: "", count: [300,220]},
+				{link: "https://www.linkedin.com/company/programydovoleb", icon: "linkedin", label: "LinkedIn", sub: "", count: [50,20]},
+				{link: "https://open.spotify.com/show/0t3PQ53sL3JkydArnDCTZZ", icon: "spotify", label: "Spotify", sub: "", count: [14,0]},
+				// {link: "https://youtube.com/@programydovoleb", icon: "yt", label: "Youtube", sub: "od července", count: [0,0]},
+				// {link: "https://discord.gg/fVUmJ7wXzc", icon: "discord", label: "Discord", sub: "nové", count: [0,]0}
 			]
 		}
 	},
