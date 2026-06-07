4d61cd5999e9298b2345e464cc3f294c96263940
Stepan <git@programydovoleb.cz>
2026-06-07 13:20:44 +0200

    Nový lineární seznam

--- src/pdv/list-linear/do.vue
+++ src/pdv/list-linear/do.vue
@@ -1,4 +1,71 @@
-<template src="./src/template.html"></template>
+<!-- <template src="./src/template.html"></template>
 <script src="./src/script.js"></script>
-<style lang="scss" scoped src="./src/style.scss"></style>
+<style lang="scss" scoped src="./src/style.scss"></style> -->
+<style>
+    .p-list-linear {
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+        list-style: none;
 
+            &.inline {
+                display: inline;
+            }
+    }
+</style>
+
+<script setup>
+    
+    import { useSlots, useAttrs, h } from 'vue'
+
+    const slots = useSlots()
+    const attrs = useAttrs()
+
+    var delimiters = [', ', ' a '];
+
+    if (attrs.class && attrs.class.includes('nebo')) {
+        delimiters = [', ', ' nebo '];
+    }
+
+    if (attrs.class && attrs.class.includes('or')) {
+        delimiters = [' · ', ' · '];
+    }
+
+    const sloted = slots.default();
+    const list = [];
+
+    function addToList (item) {
+        if (list.length > 0 && list[list.length - 1] !== delimiters[0]) {
+            list.push(h('span', {class: 'dvd'}, delimiters[0]));
+        }
+        list.push(item);
+    }
+
+    // console.log(sloted.map(x => typeof x.type));
+
+    sloted.forEach(vnode => {
+        if (typeof vnode.children === 'string' || vnode.patchFlag === -2) {
+            // nothing
+        } else if (typeof vnode.type === 'string') {
+            addToList(h(vnode)); 
+       } else {
+            console.log(vnode.children);
+            vnode.children.forEach(cnode => {
+                addToList(h(cnode));
+            })
+       } 
+    });
+
+    if (list.length > 1) {
+        list[list.length - 2] = h('span', {class: 'dvd'}, delimiters[1]);
+    }
+
+    const listnode = h('ul', {class: 'p-list-linear'}, list);
+
+</script>
+
+<template>
+    <!-- <ul class="p-list-linear" role="list"> -->
+        <component :is="listnode" role="list"></component>
+    <!-- </ul> -->
+</template>
