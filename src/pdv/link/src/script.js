e641d80824fea5a7587a33954598c6e36bfe7f26
Štěpán Ryšavý @ Programy do voleb <stepan.rysavy@programydovoleb.cz>
2026-07-07 14:33:27 +0200

    UTM override na Programy

--- src/pdv/link/src/script.js
+++ src/pdv/link/src/script.js
@@ -22,6 +22,31 @@ export default {
 			if (this.href && this.href.split('http').length === 1 && this.href.split('mailto:').length === 1) {
 				link = 'http://' + this.href;
 			}
+
+			// console.log(link);
+
+			var url = URL.parse(link);
+			var query = url.search != "" ? url.search.split('?')[1].split('&').map(x => x.split('=')) : [];
+
+			if (query.length > 0) {
+				var qutm = query.find(x => x[0] === 'utm_source');
+				if (qutm) {
+					qutm[1] = 'programydovoleb.cz';
+				} else {
+					query.push(['utm_source', 'programydovoleb.cz']);
+				}
+
+				var qarr = [];
+
+				query.forEach(x => qarr.push(x.join('=')));
+
+				link = url.origin + url.pathname + '?' + qarr.join('&');
+			} else {
+				link = url.origin + url.pathname + '?utm_source=programydovoleb.cz';
+			} 
+
+			// console.log('=>', link);
+
 			return link;
 		},
 		error: function () {
