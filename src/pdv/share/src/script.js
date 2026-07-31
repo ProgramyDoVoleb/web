c2634c4a171de62b12b3d47f75a7b730477ef86b
Štěpán Ryšavý @ Programy do voleb <stepan.rysavy@programydovoleb.cz>
2026-07-31 18:17:24 +0200

    Zkracovátko

--- src/pdv/share/src/script.js
+++ src/pdv/share/src/script.js
@@ -1,23 +1,49 @@
 import copy from 'copy-to-clipboard';
 import {ge} from '@/pdv/analytics'
+import {url} from '@/pdv/helpers'
 
 export default {
 	name: 'share-block',
-	props: ['copy', 'title', 'middle', 'link', 'cta', 'compact'],
+	props: ['copy', 'title', 'middle', 'link', 'cta', 'compact', 'pointer'],
 	data: function () {
 		return {
 			copied: false,
-			_backupCopy: 'Podívejte se na Programy do voleb'
+			_backupCopy: 'Podívejte se na Programy do voleb',
+			type: 1,
+			txt: null
 		}
 	},
 	computed: {
 		path: function () {
 			return 'https://' + window.location.hostname + (this.link || this.$route.fullPath.split('#')[0]);
+		},
+		shorthand: function () {
+			var s = [];
+
+			if (this.pointer) {
+				var p = this.pointer.split(':');
+
+				if (p[0] === 'csu_senat_rk') s.push('sk' + p[1]);
+				if (p[0] === 'csu_kv_rk') s.push('ok' + p[1]);
+				if (p[0] === 'csu_kv_ros') s.push('os' + p[1]);
+			}
+
+			if (this.type === 2 && this.title) {
+				// console.log(this.title);
+				s.push(url(this.title))
+			}
+
+			if (this.type === 3 && this.txt && this.txt.length > 0) {
+				// console.log(this.title);
+				s.push(url(this.txt))
+			}
+
+			return "http://dovoleb.cz/" + s.reverse().join('--');
 		}
 	},
 	methods: {
-		copyLink: function () {
-			copy(this.path);
+		copyLink: function (path) {
+			copy(path || this.path);
 			this.copied = true;
 
 			this.report('copy');
