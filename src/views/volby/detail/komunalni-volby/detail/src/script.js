cd4b72febd2fd54c4bb7b7a6a76ed8122fcefd52
Stepan <git@programydovoleb.cz>
2026-09-25 23:12:00 +0200

    Opravy a notifikace

--- src/views/volby/detail/komunalni-volby/detail/src/script.js
+++ src/views/volby/detail/komunalni-volby/detail/src/script.js
@@ -1,7 +1,7 @@
 import {useData} from '@/stores/data';
 import { cdn, today } from '@/stores/core';
 import { useEnums } from '@/stores/enums';
-import {url, date, number, truncate, con, gradient, color, pct, sortBy, isWoman, type, townInRegularElectionsAlsoVotesInSenat, partyInCis} from '@/pdv/helpers';
+import {url, slide, date, number, truncate, con, gradient, color, pct, sortBy, isWoman, type, townInRegularElectionsAlsoVotesInSenat, partyInCis} from '@/pdv/helpers';
 import ReportModal from '@/components/report-modal/do.vue';
 import ElectionTable from '@/components/results/parties/table/do.vue';
 import ElectionGraph from '@/components/results/parties/graph/do.vue';
@@ -93,6 +93,7 @@ export default {
 		type,
 		townInRegularElectionsAlsoVotesInSenat,
 		partyInCis,
+		slide,
 		colorByItem: function (item, data) {
 
 			var res = con(item.$data, 'color');
